c29ff9d4f27c2cf7b796f489f114d2c2e20d722c
Yoan Busca <buscayoan@hotmail.fr>
2021-02-02 16:16:06 +0100

    Premiere relecture: Package Business

--- src/main/java/business/Critere.java
+++ src/main/java/business/Critere.java
@@ -3,6 +3,8 @@ package business;
 import java.util.HashMap;
 import java.util.Map;
 
+// Enum des critere 6 possibilites. Critere utilisé dans le package Amak
+
 public enum Critere {
 	Isolement(0), Stabilite_etat(1), Stabilite_position(2), Heterogeneite(3), Murissement(4), FIN(5);
 	private int value;
@@ -19,12 +21,11 @@ public enum Critere {
     }
 
     public static Critere valueOf(int critere) {
-        return (Critere) map.get(critere);
+        return map.get(critere);
     }
 
     public int getValue() {
         return value;
     }
-	
-	
 }
+
