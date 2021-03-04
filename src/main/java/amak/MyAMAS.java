cc4f100c7942ac8a654f5a1b27824e3677400784
gogonouze <ugo.eblevadoux@g-mail.fr>
2021-03-04 17:37:18 +0100

    :recycle: refactor: Add Jdoc and upgrade the code quality

--- src/main/java/amak/MyAMAS.java
+++ src/main/java/amak/MyAMAS.java
@@ -1,90 +1,101 @@
 package amak;
 
-import application.Controller;	
+import application.Controller;
 
 import business.Blob;
 import fr.irit.smac.amak.Amas;
 import fr.irit.smac.amak.Scheduling;
 import fr.irit.smac.amak.tools.Log;
 
+/**
+ * Imaginary blob.
+ * @author inconnu, [ugo](https://github.com/gogonouze)
+ * @version 1.0
+ * @see Amas<MyEnvironment>
+ */
+public class MyAMAS extends Amas<MyEnvironment> {
 
-public class MyAMAS extends Amas<MyEnvironment>{
-	
-	private Controller controller;
-	
-	// genere des coordonn�es cart�siennes aleatoires dans carr� de cot� 100
-	public double[] genererCoordonneeAleaDansCercle() {
-		double[] res = new double[2];
-		res[0] = Math.random() * (100);
-		res[1] = Math.random() * (100);
-		return res;
-	}
-	
-	@Override
-	protected void onInitialConfiguration() {
-		int nbBlobs = (int) params[1];
-		Migrant migrant;
-		//double xcor;
-		//double ycor;
-		Blob blob;
-		controller = (Controller) params[0];		
-		for(int i = nbBlobs ; i > 0 ; i--){
-			
-			// si dans un cercle
-			double[] coo = genererCoordonneeAleaDansCercle();
-			blob = new Blob(coo[0],coo[1]);
+  /**
+   * The controller of the amas.
+   */
+  private Controller controller;
 
-			
-			// si dans un carr� :
-			//xcor = Math.random() * ( 100 );
-			//ycor = Math.random() * ( 100 );
-			//blob = new Blob(xcor,ycor, true);
-			
-			migrant = new Migrant(this, blob, controller);
-			getEnvironment().addMigrant(migrant);
-		}
-		super.onInitialConfiguration();
-		System.out.println("fin de l'initilisation de MyAmas");
-	}
-	
-	public MyAMAS(MyEnvironment env, Controller controller, int nbBlobs) {
-		super(env, Scheduling.DEFAULT, controller, nbBlobs);
-	}
+  /**
+   *
+   * @return random coordinate in a hundred by hundred square
+   */
+  public double[] genererCoordonneeAleaDansCercle() {
+    final int hundred = 100;
+    double[] res = new double[2];
+    res[0] = Math.random() * (hundred);
+    res[1] = Math.random() * (hundred);
+    return res;
+  }
 
-	
-	protected void moveAgent(Blob b, BlobAgent agent){
-//		Platform.runLater(new Runnable() {
-//			public void run() {
-				agent.setBlob(b);
-				
-				// normalement, pr�vient donc l'environnement
-//			}
-//		});
-	}
-	
-	protected void removeAgent(Blob b, BlobAgent agent){
-//		Platform.runLater(new Runnable() {
-//			public void run() {
-				getEnvironment().getAgents().remove(agent);
-				//agents.remove(agent);
-				// normalement, pr�vient donc l'environnement
-//			}
-//		});
-		
-	}
-	
-	public Controller getController() {
-		return controller;
-	}
-	
-	@Override
-	protected void onSystemCycleEnd() {
-		Log.debug("quela", "cycle end");
-	}
-	
-	@Override
-	protected void onUpdateRender() {
-		controller.updateRender();
-	}
-	
+  /**
+   * Initialisation.
+   */
+  @Override
+  protected void onInitialConfiguration() {
+    int nbBlobs = (int) params[1];
+    Migrant migrant;
+    //double xcor;
+    //double ycor;
+    Blob blob;
+    controller = (Controller) params[0];
+    for (int i = nbBlobs; i > 0; i--) {
+
+      // si dans un cercle
+      double[] coo = genererCoordonneeAleaDansCercle();
+      blob = new Blob(coo[0], coo[1]);
+
+
+      // si dans un carr� :
+      //xcor = Math.random() * ( 100 );
+      //ycor = Math.random() * ( 100 );
+      //blob = new Blob(xcor,ycor, true);
+
+      migrant = new Migrant(this, blob, controller);
+      getEnvironment().addMigrant(migrant);
+    }
+    super.onInitialConfiguration();
+    System.out.println("fin de l'initilisation de MyAmas");
+  }
+
+  /**
+   * Constructor.
+   * @param env
+   * @param pController
+   * @param nbBlobs
+   */
+  public MyAMAS(final MyEnvironment env,
+                final Controller pController, final int nbBlobs) {
+    super(env, Scheduling.DEFAULT, pController, nbBlobs);
+  }
+
+  /**
+   *
+   * @return controller
+   */
+  @Deprecated
+  public Controller getController() {
+    return controller;
+  }
+
+  /**
+   * Print "cycle end".
+   */
+  @Override
+  protected void onSystemCycleEnd() {
+    Log.debug("quela", "cycle end");
+  }
+
+  /**
+   * Update render.
+   */
+  @Override
+  protected void onUpdateRender() {
+    controller.updateRender();
+  }
+
 }
